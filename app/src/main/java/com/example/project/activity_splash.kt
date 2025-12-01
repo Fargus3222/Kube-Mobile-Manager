3b852e7fe050a1f8065c7c7dac91bc5bf743d65d
fargus3222 <fargus3222@gmail.com>
2025-12-01 21:02:15 +0300

    Add splash screen & reorder main menu

---
+++ app/src/main/java/com/example/project/activity_splash.kt
@@ -0,0 +1,36 @@
+package com.example.project
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val logo = findViewById<ImageView>(R.id.iv_logo)
+        val anim = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
+        logo.startAnimation(anim)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
